5ca69107efce46cab0938164d5575e262b9a76d5
Nazrul Islam Ridwan <md.nazrul.islam.professional@gmail.com>
2024-08-10 09:35:28 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // Function to add 'loaded' class after the page is fully loaded
 const handlePageLoad = () => {
